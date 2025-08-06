5af26ff34a8df81c64b723f73bd5070d2acc4690
黄官华 <13044238065@163.com>
2025-08-06 15:42:10 +0800

    新增useEffect状态管理

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
 import { Image } from 'expo-image';
 import {View,Text, Platform, StyleSheet } from 'react-native';
-import { useState } from 'react';
+import { useState ,useEffect} from 'react';
 
 export default function App() {
     const [userInfo, setUserIfno] = useState({})
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/jadensun')
+        .then(res => res.json())
+    }, []);
   return (
     <View>
      <Text onPress={() => setUserIfno({name: 'Jane'})}>App</Text>
