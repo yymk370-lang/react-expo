eddfde9a99c8246830921cf7b2f2dd4193432323
黄官华 <13044238065@163.com>
2025-08-06 15:39:40 +0800

    新增状态管理

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import { Image } from 'expo-image';
 import {View,Text, Platform, StyleSheet } from 'react-native';
-
+import { useState } from 'react';
 
 export default function App() {
+    const [userInfo, setUserIfno] = useState({})
   return (
     <View>
-     <Text>App</Text>
+     <Text onPress={() => setUserIfno({name: 'Jane'})}>App</Text>
     </View>
   );
 }
